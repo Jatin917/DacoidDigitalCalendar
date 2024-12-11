28fa3e4b93e0481ec0f91c1c4707ae66895c8669
Jatin917 <jkchandel02@gmail.com>
2024-12-11 12:36:51 +0530

    Project Initialized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className='bg-red-500 text-3xl text-white w-10 h-10'>
+      <p className="">
+        hello jattu
+      </p>
+      Hello Jatin
+    </div>
+  )
+}
+
+export default App
